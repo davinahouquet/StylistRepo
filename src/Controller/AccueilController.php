762ad6250d5928f371cf17f24acc533beb997759
davinahouquet <davina.houquet@hotmail.com>
2024-03-13 10:42:08 +0100

    Cartes nouveautés

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class AccueilController extends AbstractController
 {
     #[Route('/accueil', name: 'app_accueil')]
-    public function index(): Response
+    public function index(ProduitRepository $produitRepository): Response
     {
+        $produits = $produitRepository->findAll();
+
         return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
+            'produits' => $produits,
         ]);
     }
 }
